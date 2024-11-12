2e66cc31c4edf3dd74416ed866458af6971b1a9e
Your Name <you@example.com>
2024-11-12 21:44:36 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const img = document.querySelectorAll('.logo img');
+    
+    img.forEach(image => {
+        let angle = 0;
+        setInterval(() => {
+            angle += 1; // Збільшуємо кут на 1 градус
+            image.style.transform = `rotate(${angle}deg)`; // Застосовуємо обертання
+        }, 16); // 16 мс, що відповідає 60 кадрам на секунду
+    });
+});
+
+
+// Створюємо спостерігач
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        // Якщо елемент потрапив у область видимості
+        if (entry.isIntersecting) {
+            entry.target.classList.add('in-view'); // Додаємо клас для анімації
+            observer.unobserve(entry.target); // Прибираємо спостереження, щоб уникнути зайвих перевірок
+        }
+    });
+}, {
+    threshold: 0.2 // Встановлюємо поріг, коли елемент буде вважатися видимим
+});
+
+// Застосовуємо спостерігач до всіх карток
+document.querySelectorAll('.specialist-card').forEach(card => {
+    observer.observe(card);
+});
